214cc0d70351e527a0cb2cd2063469d63f9d32a9
José Garção <garcao.random@gmail.com>
2024-10-07 14:39:08 +0100

    add aave interface

---
+++ src/interfaces/IAavePool.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.27;
+
+interface IAavePool {
+    function supply(address asset, uint256 amount, address onBehalfOf, uint16 referralCode) external;
+
+    function withdraw(address asset, uint256 amount, address to) external returns (uint256);
+}
