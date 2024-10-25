a8a390bb60535aef71e4d7c9d12f57c086539503
José Garção <garcao.random@gmail.com>
2024-10-25 11:50:18 +0100

    add TwabController increase total supply unit concrete tests

---
+++ test/unit/concrete/twab-controller/increaseTotalSupply.t.sol
@@ -0,0 +1,190 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.27;
+
+import {TwabController_Unit_Shared_Test} from "../../shared/TwabController.t.sol";
+import {MIN_DEPOSIT, MAX_CARDINALITY, PERIOD_LENGTH} from "@lucky-me/utils/Constants.sol";
+import {RingBufferLib} from "@lucky-me/libraries/RingBufferLib.sol";
+import {AccountDetails} from "@lucky-me/utils/Structs.sol";
+
+contract IncreaseTotalSupply_Unit_Concrete_Test is TwabController_Unit_Shared_Test {
+    // ================================== SETUP MODIFIERS ==================================
+
+    modifier whenCardinalityIsZero() {
+        vm.startPrank(address(pool));
+        increaseAmount = MIN_DEPOSIT;
+        _;
+    }
+
+    modifier whenCardinalityBelowMax() {
+        vm.startPrank(address(pool));
+        increaseAmount = MIN_DEPOSIT;
+        twabController.increaseBalance(bob, increaseAmount);
+        _;
+    }
+
+    modifier whenNewPeriod() {
+        skipLength = PERIOD_LENGTH;
+        skip(skipLength);
+        _;
+    }
+
+    modifier whenNotNewPeriod() {
+        skipLength = PERIOD_LENGTH / 2;
+        skip(skipLength);
+        _;
+    }
+
+    modifier whenMaxCardinality() {
+        vm.startPrank(address(pool));
+        increaseAmount = MIN_DEPOSIT;
+        for (uint256 i; i < MAX_CARDINALITY; ++i) {
+            twabController.increaseBalance(bob, increaseAmount);
+            if (i != MAX_CARDINALITY - 1) skip(PERIOD_LENGTH);
+        }
+        _;
+    }
+
+    // ==================================== HAPPY TESTS ====================================
+
+    function test_IncreaseTotalSupply_ZeroCardinality() public whenCardinalityIsZero {
+        // Increase total supply
+        twabController.increaseBalance(bob, increaseAmount);
+
+        // **** TOTAL SUPPLY UPDATE ****
+        AccountDetails memory totalSupply = twabController.getTotalSupplyAccount();
+
+        // Asserting that the total supply balance was updated
+        assertEq(totalSupply.balance, increaseAmount);
+        // Asserting that the total supply next observation index was updated
+        assertEq(totalSupply.nextObservationIndex, 1);
+        // Asserting that the total supply cardinality was updated
+        assertEq(totalSupply.cardinality, 1);
+        // Asserting that a new observation was recorded
+        uint256 totalSupplyNewestIndex = RingBufferLib.newestIndex(totalSupply.nextObservationIndex, MAX_CARDINALITY);
+        assertEq(totalSupplyNewestIndex, 0);
+        // Asserting that the new observation balance was updated
+        assertEq(totalSupply.observations[0].balance, totalSupply.balance);
+        // Asserting that the new observation cumulative balance was updated
+        assertEq(totalSupply.observations[0].cumulativeBalance, 0);
+        // Asserting that the new observation timestamp was updated
+        assertEq(totalSupply.observations[0].timestamp, block.timestamp);
+    }
+
+    function test_IncreaseTotalSupply_NewPeriodWithCardinalityBelowMax() public whenCardinalityBelowMax whenNewPeriod {
+        // Get total supply account before increase balance
+        AccountDetails memory totalSupplyBefore = twabController.getTotalSupplyAccount();
+        // Compute expected new total supply
+        uint256 expectedNewBalance = totalSupplyBefore.balance + increaseAmount;
+        // Get newest observation index
+        uint256 totalSupplyNewestIndexBefore =
+            RingBufferLib.newestIndex(totalSupplyBefore.nextObservationIndex, MAX_CARDINALITY);
+        // Compute expected cumulative balance
+        uint256 expectedCumulativeBalance = totalSupplyBefore.observations[totalSupplyNewestIndexBefore]
+            .cumulativeBalance + totalSupplyBefore.observations[totalSupplyNewestIndexBefore].balance * skipLength;
+
+        // Increase total supply
+        twabController.increaseBalance(bob, increaseAmount);
+
+        // **** TOTAL SUPPLY UPDATE ****
+        AccountDetails memory totalSupplyAfter = twabController.getTotalSupplyAccount();
+
+        // Asserting that the total supply balance was updated
+        assertEq(totalSupplyAfter.balance, expectedNewBalance);
+        // Asserting that the total supply next observation index was updated
+        assertEq(totalSupplyAfter.nextObservationIndex, totalSupplyBefore.nextObservationIndex + 1);
+        // Asserting that the total supply cardinality was updated
+        assertEq(totalSupplyAfter.cardinality, totalSupplyBefore.cardinality + 1);
+        // Asserting that a new observation was recorded
+        uint256 totalSupplyNewestIndexAfter =
+            RingBufferLib.newestIndex(totalSupplyAfter.nextObservationIndex, MAX_CARDINALITY);
+        assertEq(totalSupplyNewestIndexAfter, totalSupplyNewestIndexBefore + 1);
+        // Asserting that the new observation balance was updated
+        assertEq(totalSupplyAfter.observations[totalSupplyNewestIndexAfter].balance, totalSupplyAfter.balance);
+        // Asserting that the new observation cumulative balance was updated
+        assertEq(
+            totalSupplyAfter.observations[totalSupplyNewestIndexAfter].cumulativeBalance, expectedCumulativeBalance
+        );
+        // Asserting that the new observation timestamp was updated
+        assertEq(totalSupplyAfter.observations[totalSupplyNewestIndexAfter].timestamp, block.timestamp);
+    }
+
+    function test_IncreaseTotalSupply_NotNewPeriodWithCardinalityBelowMax()
+        public
+        whenCardinalityBelowMax
+        whenNotNewPeriod
+    {
+        // Get total supply account before increase balance
+        AccountDetails memory totalSupplyBefore = twabController.getTotalSupplyAccount();
+        // Compute expected new total supply
+        uint256 expectedNewBalance = totalSupplyBefore.balance + increaseAmount;
+        // Get newest observation index
+        uint256 totalSupplyNewestIndexBefore =
+            RingBufferLib.newestIndex(totalSupplyBefore.nextObservationIndex, MAX_CARDINALITY);
+        // Compute expected cumulative balance
+        uint256 expectedCumulativeBalance = totalSupplyBefore.observations[totalSupplyNewestIndexBefore]
+            .cumulativeBalance + totalSupplyBefore.observations[totalSupplyNewestIndexBefore].balance * skipLength;
+
+        // Increase total supply
+        twabController.increaseBalance(bob, increaseAmount);
+
+        // **** TOTAL SUPPLY UPDATE ****
+        AccountDetails memory totalSupplyAfter = twabController.getTotalSupplyAccount();
+
+        // Asserting that the total supply balance was updated
+        assertEq(totalSupplyAfter.balance, expectedNewBalance);
+        // Asserting that the total supply next observation index remained the same
+        assertEq(totalSupplyAfter.nextObservationIndex, totalSupplyBefore.nextObservationIndex);
+        // Asserting that the total supply cardinality remained the same
+        assertEq(totalSupplyAfter.cardinality, totalSupplyBefore.cardinality);
+        // Asserting that the last observation was overridden
+        uint256 totalSupplyNewestIndexAfter =
+            RingBufferLib.newestIndex(totalSupplyAfter.nextObservationIndex, MAX_CARDINALITY);
+        assertEq(totalSupplyNewestIndexAfter, totalSupplyNewestIndexBefore);
+        // Asserting that the new observation balance was updated
+        assertEq(totalSupplyAfter.observations[totalSupplyNewestIndexAfter].balance, totalSupplyAfter.balance);
+        // Asserting that the new observation cumulative balance was updated
+        assertEq(
+            totalSupplyAfter.observations[totalSupplyNewestIndexAfter].cumulativeBalance, expectedCumulativeBalance
+        );
+        // Asserting that the new observation timestamp was updated
+        assertEq(totalSupplyAfter.observations[totalSupplyNewestIndexAfter].timestamp, block.timestamp);
+    }
+
+    function test_IncreaseTotalSupply_NewPeriodWithMaxCardinality() public whenMaxCardinality whenNewPeriod {
+        // Get total supply account before increase balance
+        AccountDetails memory totalSupplyBefore = twabController.getTotalSupplyAccount();
+        // Compute expected new total supply
+        uint256 expectedNewBalance = totalSupplyBefore.balance + increaseAmount;
+        // Get newest observation index
+        uint256 totalSupplyNewestIndexBefore =
+            RingBufferLib.newestIndex(totalSupplyBefore.nextObservationIndex, MAX_CARDINALITY);
+        // Compute expected cumulative balance
+        uint256 expectedCumulativeBalance = totalSupplyBefore.observations[totalSupplyNewestIndexBefore]
+            .cumulativeBalance + totalSupplyBefore.observations[totalSupplyNewestIndexBefore].balance * skipLength;
+
+        // Increase total supply
+        twabController.increaseBalance(bob, increaseAmount);
+
+        // **** TOTAL SUPPLY UPDATE ****
+        AccountDetails memory totalSupplyAfter = twabController.getTotalSupplyAccount();
+
+        // Asserting that the total supply balance was updated
+        assertEq(totalSupplyAfter.balance, expectedNewBalance);
+        // Asserting that the total supply next observation index was updated
+        assertEq(totalSupplyAfter.nextObservationIndex, 1);
+        // Asserting that the total supply cardinality remained the `MAX_CARDINALITY`
+        assertEq(totalSupplyAfter.cardinality, MAX_CARDINALITY);
+        // Asserting that a new observation was recorded at index 0
+        uint256 totalSupplyNewestIndexAfter =
+            RingBufferLib.newestIndex(totalSupplyAfter.nextObservationIndex, MAX_CARDINALITY);
+        assertEq(totalSupplyNewestIndexAfter, 0);
+        // Asserting that the new observation balance was updated
+        assertEq(totalSupplyAfter.observations[totalSupplyNewestIndexAfter].balance, totalSupplyAfter.balance);
+        // Asserting that the new observation cumulative balance was updated
+        assertEq(
+            totalSupplyAfter.observations[totalSupplyNewestIndexAfter].cumulativeBalance, expectedCumulativeBalance
+        );
+        // Asserting that the new observation timestamp was updated
+        assertEq(totalSupplyAfter.observations[totalSupplyNewestIndexAfter].timestamp, block.timestamp);
+    }
+}
