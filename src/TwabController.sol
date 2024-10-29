1a0e850eab3ae8111732e9a2bf5ea51d8fbfabb8
José Garção <garcao.random@gmail.com>
2024-10-29 12:00:56 +0000

    add TwabController get twab between function

--- src/TwabController.sol
+++ src/TwabController.sol
@@ -11,7 +11,9 @@ import {
     DECREASE_BALANCE__INSUFFICIENT_BALANCE,
     DECREASE_BALANCE__INVALID_AMOUNT,
     INCREASE_BALANCE__INVALID_AMOUNT,
-    TWAB__INVALID_PERIOD_OFFSET
+    TWAB__INVALID_PERIOD_OFFSET,
+    TWAB_BETWEEN__INVALID_TIME_RANGE,
+    TWAB_BETWEEN__ISUFFICIENT_HISTORY
 } from "./utils/Errors.sol";
 
 contract TwabController is ITwabController, Ownable {
@@ -54,6 +56,114 @@ contract TwabController is ITwabController, Ownable {
         emit ObservationRecorded(_account, observation, isNewObservation);
 
         return newBalance;
+    }
+
+    function getTwabBetween(address _account, uint256 _startTime, uint256 _endTime) external view returns (uint256) {
+        require(_startTime <= _endTime, TWAB_BETWEEN__INVALID_TIME_RANGE());
+
+        AccountDetails memory account = accounts[_account];
+
+        Observation memory endObservation = _getPreviousOrAtObservation(account, _endTime);
+
+        if (_startTime == _endTime) return endObservation.balance;
+
+        Observation memory startObservation = _getPreviousOrAtObservation(account, _startTime);
+
+        if (startObservation.timestamp != _startTime) {
+            startObservation = _calculateTemporaryObservation(startObservation, _startTime);
+        }
+
+        if (endObservation.timestamp != _endTime) {
+            endObservation = _calculateTemporaryObservation(endObservation, _endTime);
+        }
+
+        return (endObservation.cumulativeBalance - startObservation.cumulativeBalance) / (_endTime - _startTime);
+    }
+
+    function _getPreviousOrAtObservation(AccountDetails memory _account, uint256 _targetTime)
+        internal
+        pure
+        returns (Observation memory)
+    {
+        if (_account.cardinality == 0) return Observation({cumulativeBalance: 0, balance: 0, timestamp: 0});
+
+        uint256 oldestIndex =
+            RingBufferLib.oldestIndex(_account.nextObservationIndex, _account.cardinality, MAX_CARDINALITY);
+        Observation memory oldestObservation = _account.observations[oldestIndex];
+
+        if (oldestObservation.timestamp > _targetTime) {
+            require(_account.cardinality < MAX_CARDINALITY, TWAB_BETWEEN__ISUFFICIENT_HISTORY());
+
+            return Observation({cumulativeBalance: 0, balance: 0, timestamp: 0});
+        }
+
+        if (_account.cardinality == 1) return oldestObservation;
+
+        uint256 newestIndex = RingBufferLib.newestIndex(_account.nextObservationIndex, _account.cardinality);
+        Observation memory newestObservation = _account.observations[newestIndex];
+
+        if (newestObservation.timestamp <= _targetTime) return newestObservation;
+
+        if (_account.cardinality == 2) return oldestObservation;
+
+        (Observation memory prevOrAtObservation,, Observation memory afterOrAtObservation,) =
+            _binarySearch(_account.observations, newestIndex, oldestIndex, _targetTime, _account.cardinality);
+
+        if (afterOrAtObservation.timestamp == _targetTime) return afterOrAtObservation;
+
+        return prevOrAtObservation;
+    }
+
+    function _binarySearch(
+        Observation[MAX_CARDINALITY] memory _observations,
+        uint256 _newestIndex,
+        uint256 _oldestIndex,
+        uint256 _targetTime,
+        uint256 _cardinality
+    )
+        internal
+        pure
+        returns (
+            Observation memory beforeOrAt,
+            uint256 beforeOrAtIndex,
+            Observation memory afterOrAt,
+            uint256 afterOrAtIndex
+        )
+    {
+        uint256 leftSide = _oldestIndex;
+        uint256 rightSide = _newestIndex < leftSide ? leftSide + _cardinality - 1 : _newestIndex;
+        uint256 currentIndex;
+
+        while (true) {
+            // TODO - replace div operator with bit shifting
+            currentIndex = (leftSide + rightSide) / 2;
+
+            beforeOrAtIndex = RingBufferLib.wrap(currentIndex, _cardinality);
+            beforeOrAt = _observations[beforeOrAtIndex];
+            uint256 beforeOrAtTimestamp = beforeOrAt.timestamp;
+
+            afterOrAtIndex = RingBufferLib.nextIndex(currentIndex, _cardinality);
+            afterOrAt = _observations[afterOrAtIndex];
+
+            bool targetAfterOrAt = _targetTime >= beforeOrAtTimestamp;
+
+            if (targetAfterOrAt && _targetTime <= afterOrAt.timestamp) break;
+
+            if (targetAfterOrAt) leftSide = currentIndex + 1;
+            else rightSide = currentIndex - 1;
+        }
+    }
+
+    function _calculateTemporaryObservation(Observation memory _observation, uint256 _timestamp)
+        internal
+        pure
+        returns (Observation memory)
+    {
+        return Observation({
+            cumulativeBalance: _observation.cumulativeBalance + _observation.balance * (_timestamp - _observation.timestamp),
+            balance: _observation.balance,
+            timestamp: _timestamp
+        });
     }
 
     function _increaseBalance(AccountDetails storage _account, uint256 _amount)
--- src/interfaces/ITwabController.sol
+++ src/interfaces/ITwabController.sol
@@ -4,4 +4,5 @@ pragma solidity ^0.8.27;
 interface ITwabController {
     function increaseBalance(address _account, uint256 _amount) external returns (uint256);
     function decreaseBalance(address _account, uint256 _amount) external returns (uint256);
+    function getTwabBetween(address _account, uint256 _startTime, uint256 _endTime) external view returns (uint256);
 }
