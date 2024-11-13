38dbb90097fa1c9bf04bca4df263d5f415c2c6d3
José Garção <garcao.random@gmail.com>
2024-11-13 14:57:23 +0000

    Add todos in AavePoolMock

--- test/mocks/AavePoolMock.sol
+++ test/mocks/AavePoolMock.sol
@@ -4,12 +4,11 @@ pragma solidity ^0.8.27;
 import {IAavePool} from "@lucky-me/interfaces/IAavePool.sol";
 
 contract AavePoolMock is IAavePool {
-    function supply(address asset, uint256 amount, address onBehalfOf, uint16 referralCode) external {
-        // supply code
-    }
+    // TODO supply code
+    function supply(address asset, uint256 amount, address onBehalfOf, uint16 referralCode) external {}
 
+    // TODO withdraw code
     function withdraw(address asset, uint256 amount, address to) external returns (uint256) {
-        // withdraw code
         return 0;
     }
 }
