1b2c6ce1416f0144f53a2eb460ee51f2d79a6082
José Garção <garcao.random@gmail.com>
2024-11-14 12:22:12 +0000

    add implementation of QuoterMock

--- test/mocks/QuoterMock.sol
+++ test/mocks/QuoterMock.sol
@@ -4,12 +4,18 @@ pragma solidity ^0.8.27;
 import {IQuoter} from "@lucky-me/interfaces/IQuoter.sol";
 
 contract QuoterMock is IQuoter {
-    // TODO quote code
-    function quoteExactOutputSingle(
-        address tokenIn,
-        address tokenOut,
-        uint24 fee,
-        uint256 amountOut,
-        uint160 sqrtPriceLimitX96
-    ) external returns (uint256 amountIn) {}
+    // mock quote for LINK/USDC
+    uint256 public constant QUOTE = 15e18;
+    // pool fee of 0.3%
+    uint256 public constant POOL_FEE = 3000;
+
+    // hardcoded functionality for LINK/USDC pair
+    function quoteExactOutputSingle(address, address, uint24, uint256 amountOut, uint160)
+        external
+        returns (uint256 amountIn)
+    {
+        uint256 amountInWithoutFee = (amountOut * QUOTE) / 1e18;
+        uint256 feeAmount = (amountInWithoutFee * POOL_FEE) / 1_000_000;
+        amountIn = (amountInWithoutFee + feeAmount) / 1e12;
+    }
 }
