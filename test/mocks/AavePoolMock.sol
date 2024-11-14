a71252fad4598ac22a3d8968a03a868e41a10581
José Garção <garcao.random@gmail.com>
2024-11-14 11:39:40 +0000

    add implementation of AavePoolMock

--- test/mocks/AavePoolMock.sol
+++ test/mocks/AavePoolMock.sol
@@ -2,13 +2,37 @@
 pragma solidity ^0.8.27;
 
 import {IAavePool} from "@lucky-me/interfaces/IAavePool.sol";
+import {ERC20Mock} from "./ERC20Mock.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IER20.sol";
 
 contract AavePoolMock is IAavePool {
-    // TODO supply code
-    function supply(address asset, uint256 amount, address onBehalfOf, uint16 referralCode) external {}
+    ERC20Mock immutable _receiptToken;
 
-    // TODO withdraw code
+    constructor(address _receiptTokenAddress) {
+        _receiptToken = ERC20Mock(_receiptTokenAddress);
+    }
+
+    function supply(address asset, uint256 amount, address onBehalfOf, uint16) external {
+        // transfer the assets from `onBehalfOf` to this contract
+        IERC20(asset).transferFrom(onBehalfOf, address(this), amount);
+        // mint receipt tokens to `onBehalfOf`
+        _receiptToken.mint(onBehalfOf, amount);
+    }
+
     function withdraw(address asset, uint256 amount, address to) external returns (uint256) {
-        return 0;
+        // check if caller has enough receipt tokens
+        require(_receiptToken.balanceOf(msg.sender) >= amount, "AAVE: not enough funds");
+        // burn receipt tokens
+        _receiptToken.burn(msg.sender, amount);
+        // transfer underlying asset to `to`
+        IERC20(asset).transfer(to, amount);
+        // return final amount withdrawn
+        return amount;
+    }
+
+    // Mock function to generate yield
+    function updateYield(address asset, address account, uint256 amount) external {
+        _receiptToken.mint(account, amount);
+        ERC20Mock(asset).mint(address(this), amount);
     }
 }
