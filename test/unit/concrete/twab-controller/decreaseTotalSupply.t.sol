4922438c71c9988315034a2fe5c4a6ee72cf8bbc
José Garção <garcao.random@gmail.com>
2024-11-14 18:20:27 +0000

    refactor TwabController unit concrete tests for increase and decrease total supply functions

--- test/unit/concrete/twab-controller/decreaseTotalSupply.t.sol
+++ test/unit/concrete/twab-controller/decreaseTotalSupply.t.sol
@@ -3,16 +3,49 @@ pragma solidity ^0.8.27;
 
 import {TwabController_Unit_Shared_Test} from "../../shared/TwabController.t.sol";
 import {MIN_DEPOSIT, MAX_CARDINALITY, PERIOD_LENGTH} from "@lucky-me/utils/Constants.sol";
+import {
+    TWAB_DECREASE_TOTAL_SUPPLY__INVALID_AMOUNT,
+    TWAB_DECREASE_TOTAL_SUPPLY__INSUFFICIENT_BALANCE
+} from "@lucky-me/utils/Errors.sol";
+import {TotalSupplyDecreased, ObservationRecorded} from "@lucky-me/utils/Events.sol";
 import {RingBufferLib} from "@lucky-me/libraries/RingBufferLib.sol";
-import {AccountDetails} from "@lucky-me/utils/Structs.sol";
+import {AccountDetails, Observation} from "@lucky-me/utils/Structs.sol";
 
 contract DecreaseTotalSupply_Unit_Concrete_Test is TwabController_Unit_Shared_Test {
     // ================================== SETUP MODIFIERS ==================================
-    modifier whenCardinalityBelowMax() {
+
+    modifier whenCallerIsNotOwner() {
+        vm.startPrank(rando);
+        _;
+    }
+
+    modifier whenCallerIsOwner() {
         vm.startPrank(address(pool));
+        _;
+    }
+
+    modifier whenAmountIsZero() {
+        decreaseAmount = 0;
+        _;
+    }
+
+    modifier whenAmountIsNotZero() {
         decreaseAmount = MIN_DEPOSIT;
+        _;
+    }
+
+    modifier whenBalanceIsInsufficient() {
+        twabController.increaseTotalSupply(decreaseAmount - 1);
+        _;
+    }
+
+    modifier whenBalanceIsSufficient() {
         increaseAmount = decreaseAmount * 2;
-        twabController.increaseBalance(bob, increaseAmount);
+        _;
+    }
+
+    modifier whenCardinalityBelowMax() {
+        twabController.increaseTotalSupply(increaseAmount);
         _;
     }
 
@@ -29,23 +62,49 @@ contract DecreaseTotalSupply_Unit_Concrete_Test is TwabController_Unit_Shared_Te
     }
 
     modifier whenMaxCardinality() {
-        vm.startPrank(address(pool));
-        decreaseAmount = MIN_DEPOSIT;
-        increaseAmount = MIN_DEPOSIT;
         for (uint256 i; i < MAX_CARDINALITY; ++i) {
-            twabController.increaseBalance(bob, increaseAmount);
-            if (i != MAX_CARDINALITY - 1) skip(PERIOD_LENGTH);
+            skip(PERIOD_LENGTH);
+            twabController.increaseTotalSupply(increaseAmount);
         }
         _;
     }
 
+    // =================================== UNHAPPY TESTS ===================================
+
+    function test_RevertWhen_CallerIsNotOwner() public whenCallerIsNotOwner {
+        vm.expectRevert();
+        twabController.decreaseTotalSupply(decreaseAmount);
+    }
+
+    function test_RevertWhen_ZeroAmount() public whenCallerIsOwner whenAmountIsZero {
+        vm.expectRevert(TWAB_DECREASE_TOTAL_SUPPLY__INVALID_AMOUNT.selector);
+        twabController.decreaseTotalSupply(decreaseAmount);
+    }
+
+    function test_RevertWhen_InsufficientBalance()
+        public
+        whenCallerIsOwner
+        whenAmountIsNotZero
+        whenBalanceIsInsufficient
+    {
+        vm.expectRevert(TWAB_DECREASE_TOTAL_SUPPLY__INSUFFICIENT_BALANCE.selector);
+        twabController.decreaseTotalSupply(decreaseAmount);
+    }
+
     // ==================================== HAPPY TESTS ====================================
 
-    function test_DecreaseTotalSupply_NewPeriodWithCardinalityBelowMax() public whenCardinalityBelowMax whenNewPeriod {
+    function test_DecreaseTotalSupply_NewPeriodWithCardinalityBelowMax()
+        public
+        whenCallerIsOwner
+        whenAmountIsNotZero
+        whenBalanceIsSufficient
+        whenCardinalityBelowMax
+        whenNewPeriod
+    {
         // Get total supply account before decrease balance
         AccountDetails memory totalSupplyBefore = twabController.getTotalSupplyAccount();
         // Compute expected new total supply
-        uint256 expectedNewBalance = totalSupplyBefore.balance - decreaseAmount;
+        uint256 expectedNewTotalSupply = totalSupplyBefore.balance - decreaseAmount;
         // Get newest observation index
         uint256 totalSupplyNewestIndexBefore =
             RingBufferLib.newestIndex(totalSupplyBefore.nextObservationIndex, MAX_CARDINALITY);
@@ -53,14 +112,28 @@ contract DecreaseTotalSupply_Unit_Concrete_Test is TwabController_Unit_Shared_Te
         uint256 expectedCumulativeBalance = totalSupplyBefore.observations[totalSupplyNewestIndexBefore]
             .cumulativeBalance + totalSupplyBefore.observations[totalSupplyNewestIndexBefore].balance * skipLength;
 
+        // Expect the `TotalSupplyDecreased` event to be emitted
+        vm.expectEmit(true, true, true, true);
+        emit TotalSupplyDecreased(decreaseAmount, expectedNewTotalSupply, block.timestamp);
+
+        // Expect the `ObservationRecorded` event to be emitted
+        Observation memory observation = Observation({
+            balance: expectedNewTotalSupply,
+            cumulativeBalance: expectedCumulativeBalance,
+            timestamp: block.timestamp
+        });
+        vm.expectEmit(true, true, true, true);
+        emit ObservationRecorded(address(0), observation, true);
+
         // Decrease total supply
-        twabController.decreaseBalance(bob, decreaseAmount);
+        uint256 newTotalSupply = twabController.decreaseTotalSupply(decreaseAmount);
+        assertEq(newTotalSupply, expectedNewTotalSupply);
 
         // **** TOTAL SUPPLY UPDATE ****
         AccountDetails memory totalSupplyAfter = twabController.getTotalSupplyAccount();
 
         // Asserting that the total supply balance was updated
-        assertEq(totalSupplyAfter.balance, expectedNewBalance);
+        assertEq(totalSupplyAfter.balance, expectedNewTotalSupply);
         // Asserting that the total supply next observation index was updated
         assertEq(totalSupplyAfter.nextObservationIndex, totalSupplyBefore.nextObservationIndex + 1);
         // Asserting that the total supply cardinality was updated
@@ -81,13 +154,16 @@ contract DecreaseTotalSupply_Unit_Concrete_Test is TwabController_Unit_Shared_Te
 
     function test_DecreaseTotalSupply_NotNewPeriodWithCardinalityBelowMax()
         public
+        whenCallerIsOwner
+        whenAmountIsNotZero
+        whenBalanceIsSufficient
         whenCardinalityBelowMax
         whenNotNewPeriod
     {
         // Get total supply account before decrease balance
         AccountDetails memory totalSupplyBefore = twabController.getTotalSupplyAccount();
         // Compute expected new total supply
-        uint256 expectedNewBalance = totalSupplyBefore.balance - decreaseAmount;
+        uint256 expectedNewTotalSupply = totalSupplyBefore.balance - decreaseAmount;
         // Get newest observation index
         uint256 totalSupplyNewestIndexBefore =
             RingBufferLib.newestIndex(totalSupplyBefore.nextObservationIndex, MAX_CARDINALITY);
@@ -95,14 +171,28 @@ contract DecreaseTotalSupply_Unit_Concrete_Test is TwabController_Unit_Shared_Te
         uint256 expectedCumulativeBalance = totalSupplyBefore.observations[totalSupplyNewestIndexBefore]
             .cumulativeBalance + totalSupplyBefore.observations[totalSupplyNewestIndexBefore].balance * skipLength;
 
+        // Expect the `TotalSupplyDecreased` event to be emitted
+        vm.expectEmit(true, true, true, true);
+        emit TotalSupplyDecreased(decreaseAmount, expectedNewTotalSupply, block.timestamp);
+
+        // Expect the `ObservationRecorded` event to be emitted
+        Observation memory observation = Observation({
+            balance: expectedNewTotalSupply,
+            cumulativeBalance: expectedCumulativeBalance,
+            timestamp: block.timestamp
+        });
+        vm.expectEmit(true, true, true, true);
+        emit ObservationRecorded(address(0), observation, false);
+
         // Decrease total supply
-        twabController.decreaseBalance(bob, decreaseAmount);
+        uint256 newTotalSupply = twabController.decreaseTotalSupply(decreaseAmount);
+        assertEq(newTotalSupply, expectedNewTotalSupply);
 
         // **** TOTAL SUPPLY UPDATE ****
         AccountDetails memory totalSupplyAfter = twabController.getTotalSupplyAccount();
 
         // Asserting that the total supply balance was updated
-        assertEq(totalSupplyAfter.balance, expectedNewBalance);
+        assertEq(totalSupplyAfter.balance, expectedNewTotalSupply);
         // Asserting that the total supply next observation index remained the same
         assertEq(totalSupplyAfter.nextObservationIndex, totalSupplyBefore.nextObservationIndex);
         // Asserting that the total supply cardinality remained the same
@@ -121,11 +211,18 @@ contract DecreaseTotalSupply_Unit_Concrete_Test is TwabController_Unit_Shared_Te
         assertEq(totalSupplyAfter.observations[totalSupplyNewestIndexAfter].timestamp, block.timestamp);
     }
 
-    function test_DecreaseTotalSupply_NewPeriodWithMaxCardinality() public whenMaxCardinality whenNewPeriod {
+    function test_DecreaseTotalSupply_NewPeriodWithMaxCardinality()
+        public
+        whenCallerIsOwner
+        whenAmountIsNotZero
+        whenBalanceIsSufficient
+        whenMaxCardinality
+        whenNewPeriod
+    {
         // Get total supply account before decrease balance
         AccountDetails memory totalSupplyBefore = twabController.getTotalSupplyAccount();
         // Compute expected new total supply
-        uint256 expectedNewBalance = totalSupplyBefore.balance - decreaseAmount;
+        uint256 expectedNewTotalSupply = totalSupplyBefore.balance - decreaseAmount;
         // Get newest observation index
         uint256 totalSupplyNewestIndexBefore =
             RingBufferLib.newestIndex(totalSupplyBefore.nextObservationIndex, MAX_CARDINALITY);
@@ -133,14 +230,28 @@ contract DecreaseTotalSupply_Unit_Concrete_Test is TwabController_Unit_Shared_Te
         uint256 expectedCumulativeBalance = totalSupplyBefore.observations[totalSupplyNewestIndexBefore]
             .cumulativeBalance + totalSupplyBefore.observations[totalSupplyNewestIndexBefore].balance * skipLength;
 
+        // Expect the `TotalSupplyDecreased` event to be emitted
+        vm.expectEmit(true, true, true, true);
+        emit TotalSupplyDecreased(decreaseAmount, expectedNewTotalSupply, block.timestamp);
+
+        // Expect the `ObservationRecorded` event to be emitted
+        Observation memory observation = Observation({
+            balance: expectedNewTotalSupply,
+            cumulativeBalance: expectedCumulativeBalance,
+            timestamp: block.timestamp
+        });
+        vm.expectEmit(true, true, true, true);
+        emit ObservationRecorded(address(0), observation, true);
+
         // Decrease total supply
-        twabController.decreaseBalance(bob, decreaseAmount);
+        uint256 newTotalSupply = twabController.decreaseTotalSupply(decreaseAmount);
+        assertEq(newTotalSupply, expectedNewTotalSupply);
 
         // **** TOTAL SUPPLY UPDATE ****
         AccountDetails memory totalSupplyAfter = twabController.getTotalSupplyAccount();
 
         // Asserting that the total supply balance was updated
-        assertEq(totalSupplyAfter.balance, expectedNewBalance);
+        assertEq(totalSupplyAfter.balance, expectedNewTotalSupply);
         // Asserting that the total supply next observation index was updated
         assertEq(totalSupplyAfter.nextObservationIndex, 1);
         // Asserting that the total supply cardinality remained the `MAX_CARDINALITY`
--- test/unit/concrete/twab-controller/increaseTotalSupply.t.sol
+++ test/unit/concrete/twab-controller/increaseTotalSupply.t.sol
@@ -3,22 +3,40 @@ pragma solidity ^0.8.27;
 
 import {TwabController_Unit_Shared_Test} from "../../shared/TwabController.t.sol";
 import {MIN_DEPOSIT, MAX_CARDINALITY, PERIOD_LENGTH} from "@lucky-me/utils/Constants.sol";
+import {TWAB_INCREASE_TOTAL_SUPPLY__INVALID_AMOUNT} from "@lucky-me/utils/Errors.sol";
+import {TotalSupplyIncreased, ObservationRecorded} from "@lucky-me/utils/Events.sol";
 import {RingBufferLib} from "@lucky-me/libraries/RingBufferLib.sol";
-import {AccountDetails} from "@lucky-me/utils/Structs.sol";
+import {AccountDetails, Observation} from "@lucky-me/utils/Structs.sol";
 
 contract IncreaseTotalSupply_Unit_Concrete_Test is TwabController_Unit_Shared_Test {
     // ================================== SETUP MODIFIERS ==================================
 
-    modifier whenCardinalityIsZero() {
+    modifier whenCallerIsNotOwner() {
+        vm.startPrank(rando);
+        _;
+    }
+
+    modifier whenCallerIsOwner() {
         vm.startPrank(address(pool));
+        _;
+    }
+
+    modifier whenAmountIsZero() {
+        increaseAmount = 0;
+        _;
+    }
+
+    modifier whenAmountIsNotZero() {
         increaseAmount = MIN_DEPOSIT;
         _;
     }
 
+    modifier whenCardinalityIsZero() {
+        _;
+    }
+
     modifier whenCardinalityBelowMax() {
-        vm.startPrank(address(pool));
-        increaseAmount = MIN_DEPOSIT;
-        twabController.increaseBalance(bob, increaseAmount);
+        twabController.increaseTotalSupply(increaseAmount);
         _;
     }
 
@@ -35,26 +53,66 @@ contract IncreaseTotalSupply_Unit_Concrete_Test is TwabController_Unit_Shared_Te
     }
 
     modifier whenMaxCardinality() {
-        vm.startPrank(address(pool));
-        increaseAmount = MIN_DEPOSIT;
         for (uint256 i; i < MAX_CARDINALITY; ++i) {
-            twabController.increaseBalance(bob, increaseAmount);
-            if (i != MAX_CARDINALITY - 1) skip(PERIOD_LENGTH);
+            skip(PERIOD_LENGTH);
+            twabController.increaseTotalSupply(increaseAmount);
         }
         _;
     }
 
+    // =================================== UNHAPPY TESTS ===================================
+
+    function test_RevertWhen_CallerNotOwner() public whenCallerIsNotOwner {
+        vm.expectRevert();
+        twabController.increaseTotalSupply(increaseAmount);
+    }
+
+    function test_RevertWhen_ZeroAmount() public whenCallerIsOwner whenAmountIsZero {
+        vm.expectRevert(TWAB_INCREASE_TOTAL_SUPPLY__INVALID_AMOUNT.selector);
+        twabController.increaseTotalSupply(increaseAmount);
+    }
+
     // ==================================== HAPPY TESTS ====================================
 
-    function test_IncreaseTotalSupply_ZeroCardinality() public whenCardinalityIsZero {
+    function test_IncreaseTotalSupply_ZeroCardinality()
+        public
+        whenCallerIsOwner
+        whenAmountIsNotZero
+        whenCardinalityIsZero
+    {
+        // Get total supply account before increase balance
+        AccountDetails memory accountBefore = twabController.getTotalSupplyAccount();
+        // Compute expected new total supply
+        uint256 expectedNewTotalSupply = accountBefore.balance + increaseAmount;
+        // Get newest observation index
+        uint256 accountNewestIndexBefore =
+            RingBufferLib.newestIndex(accountBefore.nextObservationIndex, MAX_CARDINALITY);
+        // Compute expected cumulative balance
+        uint256 expectedCumulativeBalance = accountBefore.observations[accountNewestIndexBefore].cumulativeBalance
+            + accountBefore.observations[accountNewestIndexBefore].balance * skipLength;
+
+        // Expect the `TotalSupplyIncreased` event to be emitted
+        vm.expectEmit(true, true, true, true);
+        emit TotalSupplyIncreased(increaseAmount, expectedNewTotalSupply, block.timestamp);
+
+        // Expect the `ObservationRecorded` event to be emitted
+        Observation memory observation = Observation({
+            balance: expectedNewTotalSupply,
+            cumulativeBalance: expectedCumulativeBalance,
+            timestamp: block.timestamp
+        });
+        vm.expectEmit(true, true, true, true);
+        emit ObservationRecorded(address(0), observation, true);
+
         // Increase total supply
-        twabController.increaseBalance(bob, increaseAmount);
+        uint256 newTotalSupply = twabController.increaseTotalSupply(increaseAmount);
+        assertEq(newTotalSupply, expectedNewTotalSupply);
 
         // **** TOTAL SUPPLY UPDATE ****
         AccountDetails memory totalSupply = twabController.getTotalSupplyAccount();
 
         // Asserting that the total supply balance was updated
-        assertEq(totalSupply.balance, increaseAmount);
+        assertEq(totalSupply.balance, expectedNewTotalSupply);
         // Asserting that the total supply next observation index was updated
         assertEq(totalSupply.nextObservationIndex, 1);
         // Asserting that the total supply cardinality was updated
@@ -70,11 +128,17 @@ contract IncreaseTotalSupply_Unit_Concrete_Test is TwabController_Unit_Shared_Te
         assertEq(totalSupply.observations[0].timestamp, block.timestamp);
     }
 
-    function test_IncreaseTotalSupply_NewPeriodWithCardinalityBelowMax() public whenCardinalityBelowMax whenNewPeriod {
+    function test_IncreaseTotalSupply_NewPeriodWithCardinalityBelowMax()
+        public
+        whenCallerIsOwner
+        whenAmountIsNotZero
+        whenCardinalityBelowMax
+        whenNewPeriod
+    {
         // Get total supply account before increase balance
         AccountDetails memory totalSupplyBefore = twabController.getTotalSupplyAccount();
         // Compute expected new total supply
-        uint256 expectedNewBalance = totalSupplyBefore.balance + increaseAmount;
+        uint256 expectedNewTotalSupply = totalSupplyBefore.balance + increaseAmount;
         // Get newest observation index
         uint256 totalSupplyNewestIndexBefore =
             RingBufferLib.newestIndex(totalSupplyBefore.nextObservationIndex, MAX_CARDINALITY);
@@ -82,14 +146,28 @@ contract IncreaseTotalSupply_Unit_Concrete_Test is TwabController_Unit_Shared_Te
         uint256 expectedCumulativeBalance = totalSupplyBefore.observations[totalSupplyNewestIndexBefore]
             .cumulativeBalance + totalSupplyBefore.observations[totalSupplyNewestIndexBefore].balance * skipLength;
 
+        // Expect the `TotalSupplyIncreased` event to be emitted
+        vm.expectEmit(true, true, true, true);
+        emit TotalSupplyIncreased(increaseAmount, expectedNewTotalSupply, block.timestamp);
+
+        // Expect the `ObservationRecorded` event to be emitted
+        Observation memory observation = Observation({
+            balance: expectedNewTotalSupply,
+            cumulativeBalance: expectedCumulativeBalance,
+            timestamp: block.timestamp
+        });
+        vm.expectEmit(true, true, true, true);
+        emit ObservationRecorded(address(0), observation, true);
+
         // Increase total supply
-        twabController.increaseBalance(bob, increaseAmount);
+        uint256 newTotalSupply = twabController.increaseTotalSupply(increaseAmount);
+        assertEq(newTotalSupply, expectedNewTotalSupply);
 
         // **** TOTAL SUPPLY UPDATE ****
         AccountDetails memory totalSupplyAfter = twabController.getTotalSupplyAccount();
 
         // Asserting that the total supply balance was updated
-        assertEq(totalSupplyAfter.balance, expectedNewBalance);
+        assertEq(totalSupplyAfter.balance, expectedNewTotalSupply);
         // Asserting that the total supply next observation index was updated
         assertEq(totalSupplyAfter.nextObservationIndex, totalSupplyBefore.nextObservationIndex + 1);
         // Asserting that the total supply cardinality was updated
@@ -110,13 +188,15 @@ contract IncreaseTotalSupply_Unit_Concrete_Test is TwabController_Unit_Shared_Te
 
     function test_IncreaseTotalSupply_NotNewPeriodWithCardinalityBelowMax()
         public
+        whenCallerIsOwner
+        whenAmountIsNotZero
         whenCardinalityBelowMax
         whenNotNewPeriod
     {
         // Get total supply account before increase balance
         AccountDetails memory totalSupplyBefore = twabController.getTotalSupplyAccount();
         // Compute expected new total supply
-        uint256 expectedNewBalance = totalSupplyBefore.balance + increaseAmount;
+        uint256 expectedNewTotalSupply = totalSupplyBefore.balance + increaseAmount;
         // Get newest observation index
         uint256 totalSupplyNewestIndexBefore =
             RingBufferLib.newestIndex(totalSupplyBefore.nextObservationIndex, MAX_CARDINALITY);
@@ -124,14 +204,28 @@ contract IncreaseTotalSupply_Unit_Concrete_Test is TwabController_Unit_Shared_Te
         uint256 expectedCumulativeBalance = totalSupplyBefore.observations[totalSupplyNewestIndexBefore]
             .cumulativeBalance + totalSupplyBefore.observations[totalSupplyNewestIndexBefore].balance * skipLength;
 
+        // Expect the `TotalSupplyIncreased` event to be emitted
+        vm.expectEmit(true, true, true, true);
+        emit TotalSupplyIncreased(increaseAmount, expectedNewTotalSupply, block.timestamp);
+
+        // Expect the `ObservationRecorded` event to be emitted
+        Observation memory observation = Observation({
+            balance: expectedNewTotalSupply,
+            cumulativeBalance: expectedCumulativeBalance,
+            timestamp: block.timestamp
+        });
+        vm.expectEmit(true, true, true, true);
+        emit ObservationRecorded(address(0), observation, false);
+
         // Increase total supply
-        twabController.increaseBalance(bob, increaseAmount);
+        uint256 newTotalSupply = twabController.increaseTotalSupply(increaseAmount);
+        assertEq(newTotalSupply, expectedNewTotalSupply);
 
         // **** TOTAL SUPPLY UPDATE ****
         AccountDetails memory totalSupplyAfter = twabController.getTotalSupplyAccount();
 
         // Asserting that the total supply balance was updated
-        assertEq(totalSupplyAfter.balance, expectedNewBalance);
+        assertEq(totalSupplyAfter.balance, expectedNewTotalSupply);
         // Asserting that the total supply next observation index remained the same
         assertEq(totalSupplyAfter.nextObservationIndex, totalSupplyBefore.nextObservationIndex);
         // Asserting that the total supply cardinality remained the same
@@ -150,11 +244,17 @@ contract IncreaseTotalSupply_Unit_Concrete_Test is TwabController_Unit_Shared_Te
         assertEq(totalSupplyAfter.observations[totalSupplyNewestIndexAfter].timestamp, block.timestamp);
     }
 
-    function test_IncreaseTotalSupply_NewPeriodWithMaxCardinality() public whenMaxCardinality whenNewPeriod {
+    function test_IncreaseTotalSupply_NewPeriodWithMaxCardinality()
+        public
+        whenCallerIsOwner
+        whenAmountIsNotZero
+        whenMaxCardinality
+        whenNewPeriod
+    {
         // Get total supply account before increase balance
         AccountDetails memory totalSupplyBefore = twabController.getTotalSupplyAccount();
         // Compute expected new total supply
-        uint256 expectedNewBalance = totalSupplyBefore.balance + increaseAmount;
+        uint256 expectedNewTotalSupply = totalSupplyBefore.balance + increaseAmount;
         // Get newest observation index
         uint256 totalSupplyNewestIndexBefore =
             RingBufferLib.newestIndex(totalSupplyBefore.nextObservationIndex, MAX_CARDINALITY);
@@ -162,14 +262,28 @@ contract IncreaseTotalSupply_Unit_Concrete_Test is TwabController_Unit_Shared_Te
         uint256 expectedCumulativeBalance = totalSupplyBefore.observations[totalSupplyNewestIndexBefore]
             .cumulativeBalance + totalSupplyBefore.observations[totalSupplyNewestIndexBefore].balance * skipLength;
 
+        // Expect the `TotalSupplyIncreased` event to be emitted
+        vm.expectEmit(true, true, true, true);
+        emit TotalSupplyIncreased(increaseAmount, expectedNewTotalSupply, block.timestamp);
+
+        // Expect the `ObservationRecorded` event to be emitted
+        Observation memory observation = Observation({
+            balance: expectedNewTotalSupply,
+            cumulativeBalance: expectedCumulativeBalance,
+            timestamp: block.timestamp
+        });
+        vm.expectEmit(true, true, true, true);
+        emit ObservationRecorded(address(0), observation, true);
+
         // Increase total supply
-        twabController.increaseBalance(bob, increaseAmount);
+        uint256 newTotalSupply = twabController.increaseTotalSupply(increaseAmount);
+        assertEq(newTotalSupply, expectedNewTotalSupply);
 
         // **** TOTAL SUPPLY UPDATE ****
         AccountDetails memory totalSupplyAfter = twabController.getTotalSupplyAccount();
 
         // Asserting that the total supply balance was updated
-        assertEq(totalSupplyAfter.balance, expectedNewBalance);
+        assertEq(totalSupplyAfter.balance, expectedNewTotalSupply);
         // Asserting that the total supply next observation index was updated
         assertEq(totalSupplyAfter.nextObservationIndex, 1);
         // Asserting that the total supply cardinality remained the `MAX_CARDINALITY`
