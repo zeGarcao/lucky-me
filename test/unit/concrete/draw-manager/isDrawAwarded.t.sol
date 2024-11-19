c9d7e367f8d63d4f8fc897ffed0dafc72f5a8dfc
José Garção <garcao.random@gmail.com>
2024-11-19 15:26:03 +0000

    add DrawManager isDrawAwarded function unit concrete test

---
+++ test/unit/concrete/draw-manager/isDrawAwarded.t.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.27;
+
+import {DrawManager_Unit_Shared_Test} from "../../shared/DrawManager.t.sol";
+import {DRAW_DURATION} from "@lucky-me/utils/Constants.sol";
+
+contract IsDrawAwarded_Unit_Concrete_Test is DrawManager_Unit_Shared_Test {
+    // ================================== SETUP MODIFIERS ==================================
+
+    modifier whenCurrentOpenDrawIdBelow2() {
+        _;
+    }
+
+    modifier whenCurrentOpenDrawIdAtOrAbove2() {
+        skip(DRAW_DURATION);
+        _;
+    }
+
+    modifier whenDrawIdNotPrevious() {
+        drawId = drawManager.getCurrentOpenDrawId();
+        _;
+    }
+
+    modifier whenDrawIdIsPrevious() {
+        drawId = drawManager.getCurrentOpenDrawId() - 1;
+        _;
+    }
+
+    modifier whenRandomnessRequestIsFulfilled() {
+        _awardDraw(drawId);
+        _;
+    }
+
+    modifier whenRandomnessRequestIsNotFulfilled() {
+        _;
+    }
+
+    // ==================================== HAPPY TESTS ====================================
+
+    function test_IsDrawClosed_NoAwardedDraws() public whenCurrentOpenDrawIdBelow2 {
+        assertFalse(drawManager.isDrawAwarded(drawId));
+    }
+
+    function test_IsDrawClosed_NotPrevious() public whenCurrentOpenDrawIdAtOrAbove2 whenDrawIdNotPrevious {
+        assertFalse(drawManager.isDrawAwarded(drawId));
+    }
+
+    function test_IsDrawClosed_PreviousWithoutRandomnessFulfilled()
+        public
+        whenCurrentOpenDrawIdAtOrAbove2
+        whenDrawIdIsPrevious
+        whenRandomnessRequestIsNotFulfilled
+    {
+        assertFalse(drawManager.isDrawAwarded(drawId));
+    }
+
+    function test_IsDrawClosed_PreviousWithRandomnessFulfilled()
+        public
+        whenCurrentOpenDrawIdAtOrAbove2
+        whenDrawIdIsPrevious
+        whenRandomnessRequestIsFulfilled
+    {
+        assertTrue(drawManager.isDrawAwarded(drawId));
+    }
+}
