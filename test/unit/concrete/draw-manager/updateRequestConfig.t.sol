5d18512a34e3e670fbebbb0022d7758bb1fd3426
José Garção <garcao.random@gmail.com>
2024-11-20 15:55:17 +0000

    add DrawManager updateRequestConfig function unit concrete test

---
+++ test/unit/concrete/draw-manager/updateRequestConfig.t.sol
@@ -0,0 +1,93 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.27;
+
+import {DrawManager_Unit_Shared_Test} from "../../shared/DrawManager.t.sol";
+import {DEFAULT_CALLBACK_GAS_LIMIT, DEFAULT_REQUEST_CONFIRMATIONS} from "@lucky-me/utils/Constants.sol";
+import {
+    DRAW_REQUEST_CONFIG__INVALID_CALLBACK_GAS_LIMIT,
+    DRAW_REQUEST_CONFIG__INVALID_REQUEST_CONFIRMATIONS
+} from "@lucky-me/utils/Errors.sol";
+import {RequestConfig} from "@lucky-me/utils/Structs.sol";
+import {RequestConfigUpdated} from "@lucky-me/utils/Events.sol";
+
+contract UpdateRequestConfig_Unit_Concrete_Test is DrawManager_Unit_Shared_Test {
+    uint32 callbackGasLimit;
+    uint16 requestConfirmations;
+    // ================================== SETUP MODIFIERS ==================================
+
+    modifier whenCallerIsNotOwner() {
+        vm.startPrank(rando);
+        _;
+    }
+
+    modifier whenCallerIsOwner() {
+        vm.startPrank(address(pool));
+        _;
+    }
+
+    modifier whenCallbackGasLimitIsZero() {
+        callbackGasLimit = 0;
+        _;
+    }
+
+    modifier whenCallbackGasLimitIsNotZero() {
+        callbackGasLimit = DEFAULT_CALLBACK_GAS_LIMIT + 1;
+        _;
+    }
+
+    modifier whenRequestConfirmationsIsZero() {
+        requestConfirmations = 0;
+        _;
+    }
+
+    modifier whenRequestConfirmationsIsNotZero() {
+        requestConfirmations = DEFAULT_REQUEST_CONFIRMATIONS + 1;
+        _;
+    }
+
+    // =================================== UNHAPPY TESTS ===================================
+
+    function test_RevertWhen_NotOwner() public whenCallerIsNotOwner {
+        // Expect revert since caller is not the owner.
+        vm.expectRevert();
+        drawManager.updateRequestConfig(callbackGasLimit, requestConfirmations);
+    }
+
+    function test_RevertWhen_CallbackGasLimitIsZero() public whenCallerIsOwner whenCallbackGasLimitIsZero {
+        // Expect revert with `DRAW_REQUEST_CONFIG__INVALID_CALLBACK_GAS_LIMIT` error.
+        vm.expectRevert(DRAW_REQUEST_CONFIG__INVALID_CALLBACK_GAS_LIMIT.selector);
+        drawManager.updateRequestConfig(callbackGasLimit, requestConfirmations);
+    }
+
+    function test_RevertWhen_RequestConfirmationsIsZero()
+        public
+        whenCallerIsOwner
+        whenCallbackGasLimitIsNotZero
+        whenRequestConfirmationsIsZero
+    {
+        // Expect revert with `DRAW_REQUEST_CONFIG__INVALID_REQUEST_CONFIRMATIONS` error.
+        vm.expectRevert(DRAW_REQUEST_CONFIG__INVALID_REQUEST_CONFIRMATIONS.selector);
+        drawManager.updateRequestConfig(callbackGasLimit, requestConfirmations);
+    }
+
+    // ==================================== HAPPY TESTS ====================================
+
+    function test_UpdateRequestConfig_StoreNewConfig()
+        public
+        whenCallerIsOwner
+        whenCallbackGasLimitIsNotZero
+        whenRequestConfirmationsIsNotZero
+    {
+        // Expect `RequestConfigUpdated` to be emitted.
+        vm.expectEmit(true, true, true, true);
+        emit RequestConfigUpdated(callbackGasLimit, requestConfirmations, block.timestamp);
+
+        // Update request config.
+        drawManager.updateRequestConfig(callbackGasLimit, requestConfirmations);
+
+        // Asserting that callback gas limit and request confirmations were updated.
+        RequestConfig memory reqConfig = drawManager.getRequestConfig();
+        assertEq(reqConfig.callbackGasLimit, callbackGasLimit);
+        assertEq(reqConfig.requestConfirmations, requestConfirmations);
+    }
+}
