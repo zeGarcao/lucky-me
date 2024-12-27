e902b4e89ba3d6ef98481fe975501defc460b673
José Garção <garcao.random@gmail.com>
2024-12-27 15:13:20 +0000

    add Pool deposit integration concrete tests

--- test/integration/shared/Pool.t.sol
+++ test/integration/shared/Pool.t.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.27;
 import {BaseTest} from "../../BaseTest.t.sol";
 
 abstract contract Pool_Integration_Shared_Test is BaseTest {
+    uint256 depositAmount;
+
     function setUp() public virtual override {
         BaseTest.setUp();
     }
